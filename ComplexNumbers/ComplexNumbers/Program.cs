ee68309ac0ea4fb106221ac12d6842c6863694bf
KilianSL <58031470+KilianSL@users.noreply.github.com>
2019-12-02 11:59:08 +0000

    new file:   ComplexNumbers/ComplexNumbers.sln 	new file:   ComplexNumbers/ComplexNumbers/App.config 	new file:   ComplexNumbers/ComplexNumbers/ComplexNumbers.csproj 	new file:   ComplexNumbers/ComplexNumbers/Program.cs 	new file:   ComplexNumbers/ComplexNumbers/Properties/AssemblyInfo.cs

---
+++ ComplexNumbers/ComplexNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    class Complex
+    {
+        private double Real;
+        private double Imaginary;
+
+        public Complex(double Re, double Im)
+        {
+            Real = Re;
+            Imaginary = Im;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
