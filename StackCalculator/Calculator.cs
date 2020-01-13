dd3862abef5bba1bf2a5794f18163b3ec9bdb6c7
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-01-13 21:44:50 +0000

    Added stack-based calculator

---
+++ StackCalculator/Calculator.cs
@@ -0,0 +1,101 @@
+﻿using System; 
+
+namespace StackCalculator
+{
+    class Calculator
+    {
+
+        static void GetInput(Stack numbers)
+        {
+            
+            string input = "";
+            Console.Write(">");
+            input = Console.ReadLine();
+            ParseInput(input, numbers);
+        }
+
+        static void ParseInput(string input, Stack numbers)
+        {
+            double x;
+            if (double.TryParse(input, out x))
+            {
+                numbers.Push(x);
+            }
+            else
+            {
+                switch (input)
+                {
+                    case "+":
+                        try
+                        {
+                            var num1 = numbers.Pop();
+                            var num2 = numbers.Pop();
+                            numbers.Push(num1 + num2);
+                            Console.WriteLine(numbers.Peek());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Not enough numbers in memory");
+                        }
+                        break;
+                    case "-":
+                        try
+                        {
+                            var num1 = numbers.Pop();
+                            var num2 = numbers.Pop();
+                            numbers.Push(num2 - num1);
+                            Console.WriteLine(numbers.Peek());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Not enough numbers in memory");
+                        }
+                        break;
+                    case "*":
+                        try
+                        {
+                            var num1 = numbers.Pop();
+                            var num2 = numbers.Pop();
+                            numbers.Push(num1 * num2);
+                            Console.WriteLine(numbers.Peek());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Not enough numbers in memory");
+                        }
+                        break;
+                    case "/":
+                        try
+                        {
+                            var num1 = numbers.Pop();
+                            var num2 = numbers.Pop();
+                            numbers.Push(num2 / num1);
+                            Console.WriteLine(numbers.Peek());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Not enough numbers in memory");
+                        }
+                        break;
+                    case "clear":
+                        Console.WriteLine("Memory Cleared");
+                        numbers.Clear();
+                        break;
+                    case "exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Syntax Error");
+                        break;
+                }
+            }
+            GetInput(numbers);
+        }
+
+        static void Main(string[] args)
+        {
+            Stack numbers = new Stack();
+            GetInput(numbers);
+        }
+    }
+}
---
+++ StackCalculator/Stack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackCalculator
+{
+    class Stack
+    {
+        //Attributes
+        private double[] Data;
+        private int Top;
+        private int Size;
+
+
+        //Constructor
+        public Stack()
+        {
+            Top = -1;
+            Data = new double[16];
+            Size = 0;
+        }
+
+        //Methods
+
+        public bool IsEmpty()
+        {
+            if (Top == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsFull()
+        {
+            if (Top == Data.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public void Push(double item)
+        {
+            if (!IsFull())
+            {
+                Top += 1;
+                Data[Top] = item;
+            }
+            else
+            {
+                Console.WriteLine("Stack Overflow Error");
+            }
+        }
+
+        public double Pop()
+        {
+            if (!IsEmpty())
+            {
+                Top -= 1;
+                return Data[Top + 1];
+            }
+            else
+            {
+                throw new Exception("Stack Underflow Error");
+            }
+        }
+
+        public void Clear()
+        {
+            Top = -1;
+        }
+        public double Peek()
+        {
+            if (!IsEmpty())
+            {
+                return Data[Top];
+            }
+            else
+            {
+                Console.WriteLine("No item to peek at");
+                return default;
+            }
+        }
+    }
+}
