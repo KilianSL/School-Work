724bfb543ea3dc246d1c95b99db91929d52d8cd6
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-01-24 09:18:31 +0000

    added graph

---
+++ Graph/Graph.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using DataStructures;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph
+{
+    class Vertex
+    {
+        public string Data;
+        public DataStructures.LinkedList<int> Connections;
+
+        public Vertex(string data, int[] c)
+        {
+            Data = data;
+            Connections = new DataStructures.LinkedList<int>(c);
+        }
+    }
+    class Graph
+    {
+        //fields
+        public int Size; //number of nodes in the graph
+        public DataStructures.LinkedList<Vertex> verticies;
+
+        //classes
+        
+
+        public Graph()
+        {
+            Size = 0;
+            verticies = new DataStructures.LinkedList<Vertex>();
+        }
+
+        //Methods
+        public void AddVertex(string data, params int[] connections)  //adds a vertex to the graph
+        {
+            verticies.Add(new Vertex(data, connections));
+            Size++;
+        }
+
+        public void AddConnections(string data, params int[] connections)  //adds connections to the specified vertex
+        {
+            int i = 0;
+            var v = verticies[i];
+            while (v.Data != data && i < verticies.Count)
+            {
+                v = verticies[i];
+                i++;
+            }
+            v.Connections.AddRange(connections);
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (var vertex in verticies.ToArray())
+            {
+                str = str + $"{vertex.Data} --> ";
+                for (int i = 0; i < vertex.Connections.Count; i++)
+                {
+                    str = str + verticies[vertex.Connections[i]].Data + " ";
+                }
+                str = str + Environment.NewLine;
+            }
+            return str;
+        }
+    }
+}
---
+++ Graph/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph();
+            g.AddVertex("a", 1, 2);
+            g.AddVertex("b", 0);
+            g.AddVertex("c", 0, 1);
+            Console.WriteLine(g.ToString());
+        }
+    }
+}
