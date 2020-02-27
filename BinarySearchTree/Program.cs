3270afe47bcf50740ddf1b7d77c0e13262ef0872
KilianSL <58031470+KilianSL@users.noreply.github.com>
2020-02-27 12:43:35 +0000

    Added test code

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -27,26 +27,45 @@ namespace BinarySearchTree
             return ret;
         }
 
-        static void TestTree(int n)
+        static double[] TestTree(int n, double[] nums)
         {
             var rand = new Random();
-            double[] nums = CreateRandomDoubleArray(n);
-            int index = rand.Next(0, n - 1);
+            
+            
             var tree = new BinaryTree(nums);
-
+            double[] times = new double[2];
+            int index = rand.Next(0, n - 1);
+            //index = n - 1;
             var timer = new System.Diagnostics.Stopwatch();
             timer.Start();
             tree.SearchTree(nums[index]);
             timer.Stop();
-            Console.WriteLine($"Binary Tree Search: {timer.ElapsedTicks} ticks");
+            //Console.WriteLine("Binary Tree Search: {0} ticks", timer.ElapsedTicks);
+            times[0] = timer.ElapsedTicks;
             timer.Restart();
             LinearSearch(nums, nums[index]);
             timer.Stop();
-            Console.WriteLine($"Linear Search: {timer.ElapsedTicks} ticks");
+            //Console.WriteLine("Linear Search: {0} ticks", timer.ElapsedTicks);
+            times[1] = timer.ElapsedTicks;
+            return times;
         }
         static void Main(string[] args)
         {
-            TestTree(10000000);
+            double[] times = new double[] { 0, 0 };
+            int n = 100000;
+            int num_iter = 100;
+            double[] nums = CreateRandomDoubleArray(n);
+            for (int i = 0; i < num_iter; i++)
+            {
+                double[] time = TestTree(n, nums);
+                times[0] += time[0];
+                times[1] += time[1];
+            }
+            times[0] = times[0] / num_iter;
+            times[1] = times[1] / num_iter;
+            Console.WriteLine("Average Binary Search: {0} ticks", times[0]);
+            Console.WriteLine("Average Linear Search: {0} ticks", times[1]);
+            Console.ReadLine();
         }
     }
 }
